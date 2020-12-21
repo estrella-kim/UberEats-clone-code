3c79b184591448c43f4077ffdbdca76b96397130
estrella-kim <puris9030@gmail.com>
2020-12-22 06:26:14 +0900

    env setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,50 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from "@nestjs/graphql";
+import * as Joi from "joi";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env.' + process.env.NODE_ENV,
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('dev')
+          .required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required()
+      })
+    }),
+    TypeOrmModule.forRoot({
+      "type": "postgres",
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      "synchronize": true,
+      "logging": true,
+      "entities": [
+        "src/entity/**/*.ts"
+      ],
+      "migrations": [
+        "src/migration/**/*.ts"
+      ],
+      "subscribers": [
+        "src/subscriber/**/*.ts"
+      ]
+    }),
+    GraphQLModule.forRoot({
+    autoSchemaFile: true,
+    })
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
